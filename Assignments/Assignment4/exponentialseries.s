da81efc18ccce8a8bde8eb643a10bdb28a6fbb9d
VijaySanthoshAkula <32711452+VijaySanthoshAkula@users.noreply.github.com>
2018-10-28 11:24:47 +0530

    Update exponentialseries.s

--- Assignments/Assignment4/exponentialseries.s
+++ Assignments/Assignment4/exponentialseries.s
@@ -3,22 +3,23 @@
 	 ENTRY 
 __main  function
 	    VLDR.F32 S3,=2;Value of X float number
-        MOV R2,#2;'n' Number of terms in series
+        MOV R2,#4;'n' Number of terms in series
         VLDR.F32 S4,=1;intermedite term holding variable. Value is float
 		VLDR.F32 S5,=1;final sum of exponential series for n elements, value is float.
 		MOV R3,#1;LPBGN counter m 
         
-LPBGN    CMP R3,R2;Compare 'm' and 'n'  
-        BLE STEP;if m < n goto STEP 
-        B stop;
+        CMP R2,#1;comparing if series size is equal to 1
+		IT EQ;
+		BEQ stop;if series size equals one then jump to stop
 		
 STEP      VMUL.F32 S4,S4,S3; S4 = S4*S3 Multiply the intermedite term with Value Of X
         VMOV.F32 S1,R3;The contents of an ARM core register R1 to a single-precision register S5.
         VCVT.F32.U32 S1, S1; U32 is for unsigned . Converts a value in a register from floating-point to a 32-bit integer. 
         VDIV.F32 S4,S4,S1;s4=s4/m 
         VADD.F32 S5,S5,S4;Sum of exponential series upto 'm' elements
-        ADD R3,R3,#1;Increment the counter variable 'm' 
-        B LPBGN;GOTO Beginning of loop 
+        ADD R3,R3,#1;Increment the counter variable 'm'  
+		CMP R3,R2;Compare 'm' and 'n'  
+        BLT STEP;if m < n goto STEP 
 		
 stop    B stop 
         ENDFUNC 
